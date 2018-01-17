5e4447dd7d04bfb208a7393e1073834bf2ded7cc
glenford <fgeorgino@gmail.com>
2018-01-17 19:41:30 +0100

    fin

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,17 @@
 #write your code here
+def echo(hello)
+   hello
+end
+
+def shout(hello_world)
+hello_world.upcase
+end
+
+def repeat(hello, repeat=2)
+
+  repeat.times.collect { hello }.join(' ')
+  end
+
+def start_of_word(hello,y)
+  hello[0..y-1]
+end
