7d494a343ecad97a40a3e89f865bc88b85fb7cb9
glenford <fgeorgino@gmail.com>
2018-01-17 18:31:53 +0100

    last

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,21 @@
 #write your code here
+def add num1, num2
+  num1 + num2
+end
+
+def subtract num1, num2
+  num1 - num2
+end
+
+=begin
+def sum (array)
+  array.each do |i| sum += i
+
+  end
+end
+=end
+def sum(array)
+  sum = 0
+  array.each{ |i| sum += i }
+  sum
+end
